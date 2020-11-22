9adb1950c0bc091614a4ba2980f7f67a6ea6286a
athanase-galatsidas <athanase.galatsidas@gmail.com>
2020-11-22 19:53:16 +0100

    basic image tracking

---
+++ Assets/Scripts/CameraCollider.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollider : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas _canvas;
+
+    private void OnEnable()
+    {
+        _canvas.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (_canvas.isActiveAndEnabled)
+        {
+            _canvas.gameObject.transform.rotation = Quaternion.LookRotation(_canvas.worldCamera.transform.forward);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("MainCamera"))
+        {
+            _canvas.gameObject.SetActive(true);
+            _canvas.worldCamera = collider.GetComponent<Camera>();
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("MainCamera"))
+        {
+            _canvas.gameObject.SetActive(false);
+        }
+    }
+}
